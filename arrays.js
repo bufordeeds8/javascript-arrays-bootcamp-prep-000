1369b455c480d4a7bae603996b790aee379ed53f
Buford Eeds <bufordeeds8@gmail.com>
2019-08-22 21:19:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
   var newArr = [element,...array]
+  return newArr
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
