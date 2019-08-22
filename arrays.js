3990470af425ac9122f8a21f308a60c450061a96
Buford Eeds <bufordeeds8@gmail.com>
2019-08-22 22:00:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,3 +38,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array) {
+  var newArr = array.slice(0, array.length -1)
+  return newArr
+}
